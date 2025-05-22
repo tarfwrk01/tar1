af2db460d7d16a23a4f24cf07aacc75a13fbb6a0
thamizhnation <thamizhnationorg@gmail.com>
2025-05-22 14:40:18 +0530

    babel s3 fixes

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+// babel.config.js
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-transform-class-static-block'
+    ]
+  };
+};
